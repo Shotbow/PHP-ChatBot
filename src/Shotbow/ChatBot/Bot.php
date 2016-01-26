144ee7c352838c9cb133ad89437ae31f125f7cae
Navarr Barnier <me@navarr.me>
2016-01-25 22:53:47 -0500

    Add !why command

--- src/Shotbow/ChatBot/Bot.php
+++ src/Shotbow/ChatBot/Bot.php
@@ -259,6 +259,7 @@ MySQL;
                 'arrow'    => [$this, 'command_arrow'],
                 'dj'       => [$this, 'command_dj'],
                 'contact'  => [$this, 'command_contact'],
+                'why'      => [$this, 'command_why'],
             ];
         }
 
@@ -597,6 +598,12 @@ MySQL;
     protected function command_contact(Shotbow_ChatBot_User $sender, $arguments)
     {
         $message = 'Some issues, like rank or payment issues, can only be fixed by [url=https://shotbow.net/forum/contact]contacting support through the "Contact Us" link[/url].  Please allow two business days for a response.';
+        $this->postMessage($message);
+    }
+
+    protected function command_why(Shotbow_ChatBot_Bot $sender, $arguments)
+    {
+        $message = 'That\'s a good question.  Why *does* Shotbow have chat?  Chat is not for live admin assistance, it\'s to help foster the wonderful Shotbow community.  Ask questions, if staff is around they\'ll answer.  You can also type !command to see what other tricks I have and the information I can give you.';
         $this->postMessage($message);
     }
 }
